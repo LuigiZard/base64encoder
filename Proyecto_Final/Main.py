0181b067f8fb348f68c8793879bc23c1c8b7f0ab
LuigiZard <62489968+LuigiZard@users.noreply.github.com>
2020-03-21 22:04:49 -0600

    Add files via upload
    Cambio 1

--- Proyecto_Final/Main.py
+++ Proyecto_Final/Main.py
@@ -5,14 +5,24 @@ Created on Wed Mar 04 17:45:27 2020
 @author: LuigiZard
 """
 
-from tkinter import filedialog
+from tkinter import filedialog 
+from PIL import ImageTk
+import PIL.Image
 from tkinter import *
 import os
 import base64
 
 def browseBtn():
+   
     filename = filedialog.askopenfilename()
     texto.insert(0, filename)
+    img = ImageTk.PhotoImage(PIL.Image.open(filename))
+    #panel = Label(ventana, image = img)
+    panel.config(image = img)
+    panel.image = img
+    #panel.pack(side = "bottom", fill = "both", expand = "yes")
+    # panel.place(x = 32, y = 128)
+
     
 def convbase64():
     path = str(texto.get())
@@ -36,17 +46,23 @@ ventana.title("Convertidor imagen a Base64")
 ventana.geometry("800x480")
 letrero = Label(ventana, text = "Imagen:")
 texto = Entry(ventana, width = 100)
+limagen = Label(ventana, text = "Vista previa")
+panel = Label(ventana, image = "")
 buscarImg = Button(ventana, text = "Elegir", command=browseBtn)
 letrero2 = Label(ventana, text = "codigo base64:")
 texto2 = Text(width = 75, height = 1)
 btnconvertir = Button(ventana, text = "Codificar", command=convbase64)
 btndecodificar = Button(ventana, text = "decodificar", command=decode64)
+btnabrirArchivo = Button(ventana, text = "Abrir archivo", command = abrirar)
+btnabrirImg = Button(ventana, text = "Abrir imagen", command = abririmg)
 letrero.place(x = 32, y = 32)
 letrero2.place(x = 16, y = 64)
+limagen.place(x = 32, y = 96)
 texto.place(x = 114, y = 35)
 texto2.place(x = 114, y = 69)
 buscarImg.place(x = 724, y = 32)
 btnconvertir.place(x = 724, y = 64)
 btndecodificar.place (x = 724, y = 96)
+panel.place(x = 32, y = 128)
 
 ventana.mainloop()
